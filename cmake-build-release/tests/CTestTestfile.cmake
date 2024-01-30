a12ff5d27bd26c78db5c5d1d042501fe98ef63bb
劲松 <gaoshuai.cgs@alibaba-inc.com>
2024-01-30 20:45:22 +0800

    chore: 删除误添的构建目录

--- cmake-build-release/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cgs/Documents/project/alicloud-httpdns-sdk-c/tests
-# Build directory: /Users/cgs/Documents/project/alicloud-httpdns-sdk-c/cmake-build-release/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(httpdns_test_test "/Users/cgs/Documents/project/alicloud-httpdns-sdk-c/cmake-build-release/build/Release/bin/httpdns_test")
-set_tests_properties(httpdns_test_test PROPERTIES  _BACKTRACE_TRIPLES "/Users/cgs/Documents/project/alicloud-httpdns-sdk-c/tests/CMakeLists.txt;14;add_test;/Users/cgs/Documents/project/alicloud-httpdns-sdk-c/tests/CMakeLists.txt;0;")
